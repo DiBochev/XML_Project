de5acd683ae8dc8f822a71940f0d7ce4763bcb26
fgg12 <fgg12@FGG>
2015-02-08 16:58:02 +0200

    fixed function save as HTML

--- src/com/xmlProject/controller/XMLParcer.java
+++ src/com/xmlProject/controller/XMLParcer.java
@@ -18,7 +18,6 @@ import javax.xml.bind.Unmarshaller;
 
 import com.xmlProject.model.Homework;
 
-
 public class XMLParcer {
 
 	private String path;
@@ -27,20 +26,20 @@ public class XMLParcer {
 	private static final String END_TAG = "</homework";
 	private static final String HTML_START = "<!DOCTYPE html><html><body><table border=\"1\" style=\"width:100%\"> <tr><td>name</td><td>task</td><td>end date</td><td>hint</td><td>platform</td></tr>";
 	private static final String HTML_END = "</table></body></html>";
-        
-        /**
-         * empty constructor 
-         */
-        public XMLParcer(){
-		
+
+	/**
+	 * empty constructor
+	 */
+	public XMLParcer() {
+
 	}
-	
+
 	/**
 	 * @param path
 	 * @throws IOException
-	 * constructor with path for xml
+	 *             constructor with path for xml
 	 */
-	public XMLParcer(String path) throws IOException{
+	public XMLParcer(String path) throws IOException {
 		stringBuilder = new StringBuilder();
 		this.path = path;
 	}
@@ -51,89 +50,99 @@ public class XMLParcer {
 	 * @throws JAXBException
 	 */
 	public ArrayList<Homework> readFile() throws IOException, JAXBException {
-		try (BufferedReader bufferedReader =  new BufferedReader(new FileReader(new File(path)))){
+		try (BufferedReader bufferedReader = new BufferedReader(new FileReader(
+				new File(path)))) {
 			String line = bufferedReader.readLine();
-		    while (line != null) {
-		    	stringBuilder.append(line);
-		        line = bufferedReader.readLine();
-		    }
+			while (line != null) {
+				stringBuilder.append(line);
+				line = bufferedReader.readLine();
+			}
 		}
 		return parceXML();
 	}
 
 	/**
-	 * @return  ArrayList of homework objects
+	 * @return ArrayList of homework objects
 	 * @throws IOException
 	 * @throws JAXBException
 	 */
 	private ArrayList<Homework> parceXML() throws IOException, JAXBException {
 		int start = 0;
-	    int fin = 0;
-	    ArrayList<Homework> array = new ArrayList<Homework>();
-	   	while (stringBuilder.indexOf(START_TAG, start + 1) != -1) {
+		int fin = 0;
+		ArrayList<Homework> array = new ArrayList<Homework>();
+		while (stringBuilder.indexOf(START_TAG, start + 1) != -1) {
 			start = stringBuilder.indexOf(START_TAG, start + 1);
-			fin =  stringBuilder.indexOf(END_TAG, fin + 1) + END_TAG.length() + 1;
+			fin = stringBuilder.indexOf(END_TAG, fin + 1) + END_TAG.length()
+					+ 1;
 			array.add(XMLToObject(stringBuilder.substring(start, fin)));
 		}
-	   	return array;
+		return array;
 	}
-	
-    /**
-     * @param xml
-     * @return homework Object from xml
-     * @throws JAXBException
-     */
-    private Homework XMLToObject(String xml) throws JAXBException {
-            JAXBContext context = JAXBContext.newInstance(Homework.class);
-            Unmarshaller un = context.createUnmarshaller();
-            StringReader s = new StringReader(xml);
-            Homework hw = (Homework) un.unmarshal(s);
-            return hw;
-    }
-    
-    /**
-     * @param array
-     * @throws JAXBException
-     * @throws IOException
-     * writes objects to xml file
-     */
-    public void writeToFile(ArrayList<Homework> array) throws JAXBException, IOException{
-    	JAXBContext context = JAXBContext.newInstance(Homework.class);
-        Marshaller m = context.createMarshaller();
-        m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
-        StringBuilder sb = new StringBuilder();
-        sb.append(HTML_START);
-        
-        OutputStream os = new FileOutputStream(this.path);
-        for (Homework homework : array) {
-        	sb.append("<tr>");
-        	sb.append("<td>" + homework.getName() + "<//td>");
-        	sb.append("<td>" + homework.getTask() + "<//td>");
-        	sb.append("<td>" + homework.getEndDate() + "<//td>");
-        	sb.append("<td>" + homework.getHint() + "<//td>");
-        	sb.append("<td>" + homework.getPlatform() + "<//td>");
-        	sb.append("<//tr>");
+
+	/**
+	 * @param xml
+	 * @return homework Object from xml
+	 * @throws JAXBException
+	 */
+	private Homework XMLToObject(String xml) throws JAXBException {
+		JAXBContext context = JAXBContext.newInstance(Homework.class);
+		Unmarshaller un = context.createUnmarshaller();
+		StringReader s = new StringReader(xml);
+		Homework hw = (Homework) un.unmarshal(s);
+		return hw;
+	}
+
+	/**
+	 * @param array
+	 * @throws JAXBException
+	 * @throws IOException
+	 *             writes objects to xml file
+	 */
+	public void writeToFile(ArrayList<Homework> array) throws JAXBException,
+			IOException {
+		JAXBContext context = JAXBContext.newInstance(Homework.class);
+		Marshaller m = context.createMarshaller();
+		m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+		OutputStream os = new FileOutputStream(this.path);
+		for (Homework homework : array) {
 			m.marshal(homework, os);
 		}
-        sb.append(HTML_END);
-        try (PrintStream out = new PrintStream(new FileOutputStream("web.html"))) {
+		saveAsHTML(array);
+	}
 
-        	OutputStreamWriter o = new OutputStreamWriter(out, "Unicode");
-        	out.write(sb.toString().getBytes());
-        	o.flush();
-        	o.close();
-        }catch (Exception e) {
-        	System.out.println("cannot save to html");
+	private void saveAsHTML(ArrayList<Homework> array) {
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append(HTML_START);
+		for (Homework homework : array) {
+			sb.append("<tr>");
+			sb.append("<td>" + homework.getName() + "<//td>");
+			sb.append("<td>" + homework.getTask() + "<//td>");
+			sb.append("<td>" + homework.getEndDate() + "<//td>");
+			sb.append("<td>" + homework.getHint() + "<//td>");
+			sb.append("<td>" + homework.getPlatform() + "<//td>");
+			sb.append("<//tr>");
+		}
+		sb.append(HTML_END);
+		try (PrintStream out = new PrintStream(new FileOutputStream("web.html"))) {
+
+			OutputStreamWriter o = new OutputStreamWriter(out, "Unicode");
+			out.write(sb.toString().getBytes());
+			o.flush();
+			o.close();
+		} catch (Exception e) {
+			System.out.println("cannot save to html");
 			e.printStackTrace();
 		}
-    }
-    
-    /**
-     * set path of xml
-     * @param path
-     */
-    public void setPath(String path){
-        this.path = path;
-    }
-    
+	}
+
+	/**
+	 * set path of xml
+	 * 
+	 * @param path
+	 */
+	public void setPath(String path) {
+		this.path = path;
+	}
+
 }
